c2576b17043ab5566a3fe8c4a4f1135fb8bd327e
mike_hall_dev <mihall6846@gmail.com>
2021-09-13 10:50:03 -0500

    added additional tests

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -1,7 +1,19 @@
 import React from "react"
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import BoxList from "./BoxList"
 
+function addTestBox(boxList, height = "5", width = "5", color = "green") {
+    const heightInput = boxList.getByLabelText("Height:")
+    const widthInput = boxList.getByLabelText("Width:")
+    const colorInput = boxList.getByLabelText("Background Color:")
+
+    fireEvent.change(heightInput, { target: { value: height } })
+    fireEvent.change(widthInput, { target: { value: width } })
+    fireEvent.change(colorInput, { target: { value: color } })
+
+    const button = boxList.getByText("Add new box")
+    fireEvent.click(button)
+}
 
 it("renders without crashing", function () {
     render(<BoxList />);
@@ -10,4 +22,17 @@ it("renders without crashing", function () {
 it("matches the current stored snapshot", function () {
     const { asFragment } = render(<BoxList />);
     expect(asFragment()).toMatchSnapshot();
+})
+
+it("removes and adds box successfully", function () {
+    const boxList = render(<BoxList />)
+    const removeBtn = boxList.getByText("X")
+
+    fireEvent.click(removeBtn)
+    expect(removeBtn).not.toBeInTheDocument();
+
+    addTestBox(boxList)
+    const removeBtn2 = boxList.getByText("X")
+    expect(removeBtn2).toBeInTheDocument();
+
 })
